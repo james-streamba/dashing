a6bb0745866afd9c38e6db1eb2e1181338596e42
James Maciver <jamesmaciver@James-MacBook-Pro.local>
2015-03-02 18:43:50 +0000

    gridster demo working

--- client/scripts/views/dashboard/dashboard.js
+++ client/scripts/views/dashboard/dashboard.js
@@ -5,7 +5,7 @@ var dashboardLeaf = require('../../models/dashboard-leaf');
 
 var DashboardView = Backbone.View.extend({
 
-  el: '.content',
+  el: 'body',
 
   template: JST['client/templates/dashboard/dashboard.hbs'],
 
@@ -14,12 +14,20 @@ var DashboardView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
+    this.initGridster();
+  },
+
+  initGridster: function(){
+
+    this.$('.gridster ul').gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [140, 140]
+    });
   },
 
   render: function() {
-    this.$el.html(this.template({
-      
-    }));
+
+    this.$el.html(this.template({}));
     return this;
   }
 
